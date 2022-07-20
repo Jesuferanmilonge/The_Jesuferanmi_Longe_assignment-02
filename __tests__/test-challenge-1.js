686b7cf42c154e22573dba7743f1c4a41d735b03
Jesuferanmi <106268614+Jesuferanmilonge@users.noreply.github.com>
2022-07-20 23:59:26 +0100

    Update test-challenge-1.js

--- __tests__/test-challenge-1.js
+++ __tests__/test-challenge-1.js
@@ -1,30 +1,13 @@
-const assignment = require('../src/assignment.js');
+function sumOfNumbers(arr){
+  var length = arr.length;
+  var sum = 0;
 
-const { sumOfNumbers } = assignment;
-const maybe = sumOfNumbers === undefined ? test.skip : test;
-
-maybe('Challenge - 1 : my sumNumbers function can sum an array of numbers', () => {
-    const numbers = [
-        68,
-        -68,
-        27,
-        94,
-        72,
-        -25,
-        -51,
-        32,
-        10,
-        64,
-        -94,
-        4,
-        34,
-        -86,
-        90,
-        81,
-        20,
-        -56,
-        -91,
-        -50
-    ];
-    expect(sumOfNumbers(numbers)).toBe(75);
+  for(var i =0; i< length; i++){
+    sum += arr[i];
+  }
+return sum; 
+}
+  const array= [68,-68,27,94,72,-25,-51,32,10,64,-94,4,34,-86,90,81,20,-56,-91,-50]
+  let sum = sumOfNumbers(array);
+  alert("sumArrayOfNumbers is " + sum)
 });
